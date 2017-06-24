d15fc3c4efad36d8099d113676ca2aba2ccc568a
Wenxin Wang <wenxin.wang94@gmail.com>
2017-06-24 21:21:15 +0800

    add nvidia-smi influxdb script

--- prep.sh
+++ prep.sh
@@ -4,5 +4,9 @@ __FILE__=$(readlink -e ${BASH_SOURCE[0]})
 EXEC_DIR=$(dirname $__FILE__)
 INSTANCE=$EXEC_DIR/instance
 
+sudo pacman -S --needed cronie python nvidia-settings
+sudo systemctl enable cronie
+sudo systemctl start cronie
+
 . $INSTANCE/venv/bin/activate
 pip install -r $EXEC_DIR/requirements.txt
